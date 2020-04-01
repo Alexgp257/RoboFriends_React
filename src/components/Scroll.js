db7779087b3b50849e0aa524e32224e52fb8433e
Alexgp257 <alex.g.pasca@gmail.com>
2020-04-01 10:50:01 +0100

    pushing the app to github

---
+++ src/components/Scroll.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './Scroll.css';
+const Scroll = (props) => {
+    return(
+        <div id='scroll'>
+            {props.children}
+        </div>
+    );
+}
+
+export default Scroll;
